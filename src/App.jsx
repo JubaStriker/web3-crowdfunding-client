6aca374465c81ff808b0b8e4c88f3463080fe375
JubaStriker <jubastriker@gmail.com>
2023-09-04 16:09:17 +0600

    All routes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import { Navbar, Sidebar } from './components';
+import Profile from './pages/Profile';
+import CreateCampaign from './pages/CReateCampaign';
+import CampaignDetails from './pages/CampaignDetails';
 
 const App = () => {
     return (
@@ -9,10 +12,13 @@ const App = () => {
             <div className='sm:flex hidden mr-10 relative'>
                 <Sidebar />
             </div>
-            <div className='flex flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+            <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/create-campaign" element={<CreateCampaign />} />
+                    <Route path="/campaign-details/:id" element={<CampaignDetails />} />
                 </Routes>
             </div>
         </div>
