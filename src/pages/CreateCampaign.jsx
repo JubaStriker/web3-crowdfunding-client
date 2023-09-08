11ae51a62cb788afd4ed18795b644681cf0096b0
JubaStriker <jubastriker@gmail.com>
2023-09-08 11:05:51 +0600

    Loader fixed

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 import { money } from '../assets';
 import { checkIfImage } from '../utils';
-import { FormField, CustomButton } from '../components';
+import { FormField, CustomButton, Loader } from '../components';
 import { useStateContext } from '../context';
 
 
@@ -42,7 +42,7 @@ const CreateCampaign = () => {
 
     return (
         <div className='bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4'>
-            {isLoading && 'Loader'}
+            {isLoading && <Loader />}
             <div className='flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]'>
                 <h1 className='font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white'>Create Campaign</h1>
             </div>
